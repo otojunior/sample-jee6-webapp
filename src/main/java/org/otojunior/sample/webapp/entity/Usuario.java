b85b8aa4d4733764b7b177b5f8c972cc32f7b176
Oto Junior <otojunior@gmail.com>
2016-04-15 17:03:20 -0300

    Acesso aos campos XML via Field

--- src/main/java/org/otojunior/sample/webapp/entity/Usuario.java
+++ src/main/java/org/otojunior/sample/webapp/entity/Usuario.java
@@ -5,6 +5,8 @@ package org.otojunior.sample.webapp.entity;
 
 import java.io.Serializable;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -15,31 +17,52 @@ import org.apache.commons.lang3.builder.ToStringStyle;
  * @author 01456231650
  *
  */
+@XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement(name = "usuario")
 public class Usuario implements Serializable {
 	private static final long serialVersionUID = 7686135950453373822L;
 	
+	@XmlElement
 	private String nome;
+
+	@XmlElement
 	private Integer idade;
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public String getNome() {
 		return nome;
 	}
 
-	@XmlElement
+	/**
+	 * 
+	 * @param nome
+	 */
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public Integer getIdade() {
 		return idade;
 	}
 
-	@XmlElement
+	/**
+	 * 
+	 * @param idade
+	 */
 	public void setIdade(Integer idade) {
 		this.idade = idade;
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String toString() {
 		return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
