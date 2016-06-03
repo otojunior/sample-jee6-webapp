5116bf0fecf5dd60fdb35ff043916fdf53f31583
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2016-06-03 17:56:53 -0300

    Retirada da classe cliente problemática

--- src/main/java/org/otojunior/sample/webapp/service/soap/IUsuarioServiceWS.java
+++ src/main/java/org/otojunior/sample/webapp/service/soap/IUsuarioServiceWS.java
@@ -3,17 +3,27 @@
  */
 package org.otojunior.sample.webapp.service.soap;
 
+import java.util.List;
+
+import javax.ejb.Local;
+import javax.ejb.Stateless;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
+
+import org.otojunior.sample.webapp.entity.Usuario;
 
 /**
  * @author 01456231650
  *
  */
+@Local
+@Stateless
 @WebService
 public interface IUsuarioServiceWS {
 	
 	@WebMethod
-	public String olaSr(String nome);
-
+	public List<Usuario> getUsuarios();
+	
+	@WebMethod
+	public Usuario getUsuario(Long id);
 }
--- src/main/java/org/otojunior/sample/webapp/service/soap/UsuarioServiceWS.java
+++ src/main/java/org/otojunior/sample/webapp/service/soap/UsuarioServiceWS.java
@@ -3,15 +3,38 @@
  */
 package org.otojunior.sample.webapp.service.soap;
 
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
 import javax.jws.WebService;
+
+import org.otojunior.sample.webapp.dao.UsuarioDao;
+import org.otojunior.sample.webapp.entity.Usuario;
 
 /**
  * @author 01456231650
  *
  */
+
 @WebService
 public class UsuarioServiceWS implements IUsuarioServiceWS {
-	public String olaSr(String nome) {
-		return "Olá Sr. " + nome;
+	@EJB
+	private UsuarioDao dao;
+	
+	/**
+	 * 
+	 */
+	public List<Usuario> getUsuarios() {
+		return dao.findAll();
+	}
+
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public Usuario getUsuario(Long id) {
+		return dao.findById(id);
 	}
 }
